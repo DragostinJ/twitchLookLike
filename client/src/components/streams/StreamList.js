c07f6b0408952f9c944343b2ff4c5f990e2c790a
unknown <dragostinj@gmail.com>
2019-01-07 07:49:52 +0200

    finished main routing of app

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -16,8 +16,11 @@ class StreamList extends React.Component {
             Edit
           </Link>
           {/* <button className="ui button primary">edit</button> */}
-          <Link to={`/streams/delete/${stream.id}`} className="ui button negative">
-          Delete
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
+            Delete
           </Link>
         </div>
       );
@@ -30,7 +33,9 @@ class StreamList extends React.Component {
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link className="header" to={`/streams/${stream.id}`}>
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-    return <div>StreamShow</div>
+class StreamShow extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id); //matching ID of the route ID
+  }
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    const {title, description} = this.props.stream
+    return (
+      <div>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
 }
+const mapStateTopProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }; // assign route ID to the db ?
+};
 
-export default StreamShow
+export default connect(
+  mapStateTopProps,
+  { fetchStream }
+)(StreamShow);
